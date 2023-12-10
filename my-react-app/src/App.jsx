135f903ebd156f1c50d0907ed8fe397b3db776f7
Mike Allen <freediver72615@gmail.com>
2023-12-09 17:45:38 -1000

    ready to connect to chatGPT

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,49 @@
-
-import React from 'react';
+import React, { useState } from 'react';
+import DataInput from './components/dataInput';
+import DisplayData from './components/displayData';
 import './App.css'
-import DataInput from './components/dataInput.jsx';
-import DisplayData from './components/displayData.jsx';
 
 function App() {
+  const [chatGPTResponse, setChatGPTResponse] = useState(null);
+
+  const handleChatGPTResponse = (data) => {
+    setChatGPTResponse(data);
+  };
 
   return (
     <div>
-      <h1>Coral Database</h1>
-      <DataInput />
-      <DisplayData />
+      <h1>Coral Species Recognition</h1>
+      <DataInput onChatGPTResponse={handleChatGPTResponse} />
+      {chatGPTResponse && <DisplayData chatGPTData={chatGPTResponse} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+// import React, { useState } from 'react';
+// import './App.css'
+// import DataInput from './components/dataInput.jsx';
+// import DisplayData from './components/displayData.jsx';
+
+// function App() {
+//   const [apiData, setApiData] = useState(null);
+
+//   const handleAPIData = (data) => {
+//     setApiData(data);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Coral AI model integration</h1>
+//       <DataInput onData={handleAPIData} />
+//       {/* <DisplayData dataFromAPI={apiData} /> //renders null and lets us see the component */}
+//       {apiData && <DisplayData dataFromAPI={apiData} />} //just to see the component */
+//     </div>
+//   )
+// }
+
+// export default App
--- my-react-app/src/components/dataInput.jsx
+++ my-react-app/src/components/dataInput.jsx
@@ -1,26 +1,132 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
+
+function DataInput({ onChatGPTResponse }) {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (selectedFile) {
+      // Prepare data to send to ChatGPT API
+      const formData = new FormData();
+      formData.append('image', selectedFile);
+
+      // Send image data to ChatGPT's API endpoint
+      try {
+        const response = await fetch('CHATGPT_API_ENDPOINT', {
+          method: 'POST',
+          body: formData,
+          // Add necessary headers like Authorization, content type, etc.
+        });
+
+        const data = await response.json();
+        // Handle the data received from ChatGPT's API
+        console.log('Data from ChatGPT:', data);
+        // Pass data to parent component (App.jsx)
+        onChatGPTResponse(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    } else {
+      console.error('No file selected');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default DataInput;
+
+
+
+
+// import React, { useState } from 'react';
+
+// function DataInput() {
+//   const [selectedFile, setSelectedFile] = useState(null);
+
+//   const handleFileChange = (e) => {
+//     setSelectedFile(e.target.files[0]);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     if (selectedFile) {
+//       // Perform API request with the selected file
+//       const formData = new FormData();
+//       formData.append('image', selectedFile);
+
+//       // Use fetch or any HTTP library to send the file to your AI model API
+//       try {
+//         const response = await fetch('AI_MODEL_API_ENDPOINT', {
+//           method: 'POST',
+//           body: formData,
+//           // Add necessary headers like Authorization, content type, etc.
+//         });
+
+//         const data = await response.json();
+//         // Handle the data received from the API as needed
+//         console.log('Data from AI model:', data);
+//         // Update state or pass this data to displayData component
+//       } catch (error) {
+//         console.error('Error:', error);
+//       }
+//     } else {
+//       console.error('No file selected');
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <input type="file" onChange={handleFileChange} />
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default DataInput;
+
+
+
+
+
+
+
+
+
+
+// //original frontend design
+// // import React from 'react'
+// // import { useState } from 'react';
 
 
   
 
-const DataInput = () => {
-  return (
-    <div>
-      <p>Data input</p>
-      <input placeholder="Scientific Name"/>
-      <br></br>
-      <input type="file" />
-      <input type="text" placeholder="enter the URL of the picture" />
-      
-      <br></br>
-      <textarea placeholder="Description of Coral"/>
-      <br></br>
+// const DataInput = () => {
+//   return (
+//     <div>
+//       <p>Upload your photo here</p>
+//       <textarea placeholder="Image URL"/>
+//       <br></br>
 
-      <button type="submit">Submit</button>
-    </div>
+//       <button type="submit">Submit</button>
+//     </div>
     
-  )
-}
+//   )
+// }
 
-export default DataInput;
+// export default DataInput;
